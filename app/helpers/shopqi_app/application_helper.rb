ed6ffa45182ecce4f031bf91f0616b31d094bf70
saberma <mahb45@gmail.com>
2012-07-01 13:13:47 +0800

    move signed_in method to helper.

--- app/helpers/shopqi_app/application_helper.rb
+++ app/helpers/shopqi_app/application_helper.rb
@@ -1,4 +1,11 @@
 module ShopqiApp
   module ApplicationHelper
+    def authenticate_shop! # 必须通过认证
+      redirect_to root_path unless signed_in?
+    end
+
+    def signed_in?
+      session[:shopqi]
+    end
   end
 end
