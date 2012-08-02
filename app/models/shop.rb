ef6f61e335deb7f86594f2db21bba8727ff27e84
saberma <mahb45@gmail.com>
2012-08-02 14:07:57 +0800

    fixed: after oauth scopes changed, access_token should be update.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,7 +2,9 @@ class Shop < ActiveRecord::Base
   attr_accessible :shop_id, :name, :shopqi_domain, :access_token
 
   def self.find_for_shopqi_oauth(data)
-    shop = data.extra.raw_info.shop
-    where(shop_id: shop.id).first_or_create! name: shop.name, shopqi_domain: shop.shopqi_domain, access_token: data.credentials.token
+    shop_data = data.extra.raw_info.shop
+    shop = where(shop_id: shop_data.id).first_or_create! name: shop_data.name, shopqi_domain: shop_data.shopqi_domain
+    shop.access_token = data.credentials.token
+    shop.tap(&:save)
   end
 end
