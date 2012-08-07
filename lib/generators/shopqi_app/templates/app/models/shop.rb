4ca982987e1fa0572015aed080fd3306b4fe4081
saberma <mahb45@gmail.com>
2012-08-07 10:37:03 +0800

    add shop methods doc.

--- lib/generators/shopqi_app/templates/app/models/shop.rb
+++ lib/generators/shopqi_app/templates/app/models/shop.rb
@@ -1,4 +1,4 @@
-require "#{ShopQiApp::Engine.models_dir}/shop"
+require "#{ShopQiApp::Engine.models_dir}/shop" # methods: use_shopkit admin_url front_url
 class Shop
-  attr_accessible :shop_id, :name, :shopqi_domain, :access_token
+  attr_accessible :shop_id, :name, :shopqi_domain
 end
